7c6017aa0544044ba6bb55d4d57dfd33b3d1b0b0
johanserna <el.johan0511@hotmail.com>
2023-07-07 16:41:57 -0500

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,36 @@
 import './App.css'
 import { useEffect} from 'react'
-import axios from 'axios'
 
 function App() {
 
   //logica de javascriptttttttttttttttttttttt
 
   
-  useEffect(()=>{
+  useEffect(() => {
     const URL = "http://190.143.102.99:28281/trazabilidad.asmx";
-
-
-    const xmlData =`<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:tem="http://tempuri.org/">
-    <soap:Header/>
-    <soap:Body>
-       <tem:GET_TRAZABILITY_SHIPPING>
-          <!--Optional:-->
-          <tem:ENVIO>RA263144718CO</tem:ENVIO>
-       </tem:GET_TRAZABILITY_SHIPPING>
-    </soap:Body>
- </soap:Envelope>`;
-
-const config = {
-  headers: {
-    'Content-Type': 'text/xml'
-
-  }
-};
-
-axios.post(URL, xmlData, config)
-  .then(res => {console.log(res.data);})
-  .catch(err => {console.log(err);});
-  },[])
+  
+    const xmlData = `<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:tem="http://tempuri.org/">
+      <soap:Header/>
+      <soap:Body>
+         <tem:GET_TRAZABILITY_SHIPPING>
+            <!--Optional:-->
+            <tem:ENVIO>RA263144718CO</tem:ENVIO>
+         </tem:GET_TRAZABILITY_SHIPPING>
+      </soap:Body>
+      </soap:Envelope>`;
+  
+    fetch(URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/xml'
+      },
+      body: xmlData
+    })
+    .then(res => res.text())
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+  }, []);
+  
    //logica de javascript
    //html
   return (
